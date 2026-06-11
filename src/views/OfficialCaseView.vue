a5a8b13d028f34b71a7992ee2066e9cf1243a3dd
钟纪杰 <10463783+zhong-jijie@user.noreply.gitee.com>
2026-06-11 16:30:32 +0800

    注释开发者工具

--- src/views/OfficialCaseView.vue
+++ src/views/OfficialCaseView.vue
@@ -1512,7 +1512,7 @@ onUnmounted(() => {
                 </div>
               </section>
 
-              <section class="developer-import-panel" aria-label="开发工具">
+              <!-- <section class="developer-import-panel" aria-label="开发工具">
                 <div>
                   <strong>开发工具</strong>
                   <span>跳过关键词检索，直接归档第一章全部线索。</span>
@@ -1527,9 +1527,9 @@ onUnmounted(() => {
                     collectedChapterClues.length === chapterClueIds.length
                       ? '全部线索已导入'
                       : '一键导入全部线索'
-                  }}
+                    }}
                 </el-button>
-              </section>
+              </section> -->
 
               <el-button
                 class="clues-replay-button"
